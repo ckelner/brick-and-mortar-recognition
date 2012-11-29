f8fa025442890586d49c3aea7f861a98e8f3ea41
Chris Kelner <chris.kelner@gmail.com>
2012-11-28 22:20:32 -0500

    centered the timeline on load so that current hour is not always to the far left.  During later hours this is undesirable

--- client/timeline.js
+++ client/timeline.js
@@ -1,10 +1,14 @@
-//145px blocks... use this to advantage...
+//144px blocks... use this to advantage...
 function setupTimeline(){
 	global_userClockTime=global_Hours;
 	$('#scrollTimeline').draggable({ axis: "x" });
 	watchDraggable();
+	//window width minus padding
+	var ww=$('#scrollTimeline').parent().parent().width()/2;
 	// set up current time
-	$('#scrollTimeline').css("left", -1*global_userClockTime*145);
+	$('#scrollTimeline').css("left", -1*global_userClockTime*144+ww);
+	//debug
+	console.log(ww);
 }
 
 function watchDraggable(){
