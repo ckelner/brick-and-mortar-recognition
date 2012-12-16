875f69d328d7e10b2b39f86cc7916f61b1d48b9e
Chris Kelner <chris.kelner@gmail.com>
2012-12-16 00:31:31 -0500

    hacky prod/qa/dev detection

--- server/publish.js
+++ server/publish.js
@@ -1,20 +1,6 @@
-MeteorEnv = function(options) {
+var isProd=false;
 
-  // Don't do anything if there's a Meteor.env already for some reason
-  if (!Meteor.env) {
-    Meteor.env = {};
-
-    var path = __meteor_bootstrap__.require('path');
-    var pathToConfig = process.env.HOME + '/.meteor.cfg';
-
-    var configExists = path.existsSync(pathToConfig);
-    Meteor.env.isDevelopment = configExists;
-    Meteor.env.isProduction = !configExists;
-
-  }  
-};
-
-if(MeteorEnv.isProduction){
+if(isProd){
 	console.log("is prod");
 	Guests = new Meteor.Collection("guests_prod");
 }else{
